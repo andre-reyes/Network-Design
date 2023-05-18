56033b3d1ad8a7c31782b0b41209e42612710eda
Areyes <areyez92@gmail.com>
2023-05-18 19:58:12 -0400

    added option to quit the process

--- Phase 1/mtchat.py
+++ Phase 1/mtchat.py
@@ -2,31 +2,30 @@ from threading import Thread
 from socket import *
 from time import sleep
 
-buffer = 2048
-
-def server():
-    local_port = 65309         # Port number chosen from range of values (49152-65535) for private/temporary purposes
-    bufferSize = 2048          # 
+def server(buffer, port):
 
     #create server socket
     server_socket = socket(AF_INET, SOCK_DGRAM)
-    server_socket.bind(("",local_port))
+    server_socket.bind(("",port))
 
-    print("The server is ready to receive on port {}".format(local_port))
+    print("The server is ready to receive on port {}".format(port))
     
 
     #infinite loop to always be listening to port while active
     while bool:
-        message, client_address = server_socket.recvfrom(bufferSize)
-        modified_message = message.decode().upper() + "\n\tclient port: {}".format(client_address[1])
-        server_socket.sendto(modified_message.encode(), client_address)
+        message, client_address = server_socket.recvfrom(buffer)
 
-        
-
-def client():
+        if message.decode().lower() == 'quit':
+            server_socket.sendto('quit'.encode(), client_address)
+            server_socket.close()
+            print("The server has ended.")
+            break
+        else:
+            modified_message = message.decode().upper() + "\n\tclient port: {}".format(client_address[1])
+            server_socket.sendto(modified_message.encode(), client_address)
+    
+def client(buffer, port):
     server_name = "localhost" # can also be 127.0.0.1
-    server_port = 65309
-    bufferSize = 2048
 
     # create socket for client
     client_socket = socket(AF_INET, SOCK_DGRAM)
@@ -34,19 +33,25 @@ def client():
     # grab message from user and send it to server
     # 
     while True:
-        message = input('Input lowercase sentence: ')
-        client_socket.sendto(message.encode(),(server_name, server_port))
-        modified_message, server_address = client_socket.recvfrom(bufferSize)
-        address, port = server_address
-
-        print ("\tmessage from server: {} \n\tserver address: {} \n\tserver port: {}\n".format(modified_message.decode(), address, port))
-
-            
+        message = input('Input lowercase sentence (Enter "quit" to end): ')    
+        client_socket.sendto(message.encode(),(server_name, port))
+        modified_message, server_address = client_socket.recvfrom(buffer)
+        address, port = server_address   
+        if modified_message.decode().lower() == 'quit':
+            client_socket.close()
+            print("The client has ended.")
+            break
+        else:
+            print ("\tmessage from server: {} \n\tserver address: {} \n\tserver port: {}\n".format(modified_message.decode(), address, port))
+        
 
 if __name__ ==  '__main__':
+    PORT = 65309         # Port number chosen from range of values (49152-65535) for private/temporary purposes
+    BUFFER = 2048
+
     print ("Creating threads...")
-    server_thread = Thread(target=server)
-    client_thread = Thread(target=client)
+    server_thread = Thread(target=server, args=(BUFFER, PORT,))
+    client_thread = Thread(target=client, args=(BUFFER, PORT,))
     
     print ("Starting threads...")
     server_thread.start()
@@ -57,7 +62,6 @@ if __name__ ==  '__main__':
     server_thread.join()
     
     print ("threads complete...")
-    server_thread.end()
-    client_thread.end()
+
 
 
