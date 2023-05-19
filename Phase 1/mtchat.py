09414c18e8ce0fa4ba3acd2168393cfec710c017
Areyes <areyez92@gmail.com>
2023-05-19 09:59:19 -0400

    Cleaned up comments and added main() function

--- Phase 1/mtchat.py
+++ Phase 1/mtchat.py
@@ -23,60 +23,17 @@ from threading import Thread
 from socket import *
 from time import sleep
 
-# Server function takes in two arguments tp assign a port and buffer size to the socket and returns the modified data as a string to client
-def Server(buffer, port):
-
-    # create server socket
-    server_socket = socket(AF_INET, SOCK_DGRAM)
-    server_socket.bind(("",port))
-    print("The server is ready to receive on port {}".format(port))
-    
-
-    # infinite loop to always be listening to port
-    while True:
-        message, client_address = server_socket.recvfrom(buffer)
-
-    # on quit: server sends one last message to client to inform client to disconnect otherwise client endless loops waiting for server to respond
-        if message.decode().lower() == 'q':
-            server_socket.sendto('q'.encode(), client_address)
-            server_socket.close()
-            print("The server has ended.")
-            break
-        else:
-            modified_message = message.decode().upper() + "\n\tclient port: {}".format(client_address[1])
-            server_socket.sendto(modified_message.encode(), client_address)
+def main():
+# main function that handles creating and running 
+# threads for the server and client processes.
 
 
-# Client function takes in two arguments to assign a port and buffer size to the client socket, sends user input to server and returns a string representation 
-def Client(buffer, port):
-    HOST = "localhost" # localhost == 127.0.0.1
-
-    # create socket for client
-    client_socket = socket(AF_INET, SOCK_DGRAM)
-
-    # grab message from user and send it to server
-    while True:
-        message = input('Input lowercase sentence (Enter "q" or "Q" to end): ')    
-        client_socket.sendto(message.encode(),(HOST, port))
-        modified_message, server_address = client_socket.recvfrom(buffer)
-        address, port = server_address   
-
-        # on quit: using the modified_message from the server otherwise the thread will continue and close causing socket error on server side
-        if modified_message.decode().lower() == 'q':
-            client_socket.close()
-            print("The client has ended.")
-            break
-        else:
-            print ("\tmessage from server: {} \n\tserver address: {} \n\tserver port: {}\n".format(modified_message.decode(), address, port))
-        
-
-if __name__ ==  '__main__':
-    PORT = 65309         # Port number chosen from range of values (49152-65535) for private/temporary purposes
-    BUFFER = 2048
+    PORT = 65309         # chosen from range of values (49152-65535) for private/temporary purposes
+    BUFFER = 2048        # size for the message, anything outside the range is lost in UDP packets
 
     print ("Creating threads...")
-    server_thread = Thread(target=Server, args=(BUFFER, PORT,))
-    client_thread = Thread(target=Client, args=(BUFFER, PORT,))
+    server_thread = Thread(target=server, args=(BUFFER, PORT,))
+    client_thread = Thread(target=client, args=(BUFFER, PORT,))
     
     print ("Starting threads...")
     server_thread.start()
@@ -87,6 +44,55 @@ if __name__ ==  '__main__':
     server_thread.join()
     
     print ("threads complete...")
+
+
+def server(buffer, port):
+# server  creates a new socket and allows 
+# sending data to/from the client socket.
+
+    server_socket = socket(AF_INET, SOCK_DGRAM)
+    server_socket.bind(("",port))
+    print("The server is ready to receive on port {}".format(port))
+
+    while True:
+        message, client_address = server_socket.recvfrom(buffer)
+
+        if message.decode().lower() == 'q':
+            server_socket.sendto('q'.encode(), client_address)
+            server_socket.close()
+            print("The server has ended.")
+            break
+        else:
+            modified_message = message.decode().upper() + "\n\tclient port: {}".format(client_address[1])
+            server_socket.sendto(modified_message.encode(), client_address)
+
+
+def client(buffer, port):
+# Client  creates a socket to send and 
+# recieve a message from/to the server.
+
+
+    HOST = "localhost" # localhost == 127.0.0.1
+
+    client_socket = socket(AF_INET, SOCK_DGRAM)
+    
+    while True:
+        message = input('Input lowercase sentence (Enter "q" or "Q" to end): ')    
+        client_socket.sendto(message.encode(),(HOST, port))
+        modified_message, server_address = client_socket.recvfrom(buffer)
+        address, port = server_address   
+
+        
+        if modified_message.decode().lower() == 'q':
+            client_socket.close()
+            print("The client has ended.")
+            break
+        else:
+            print ("\tmessage from server: {} \n\tserver address: {} \n\tserver port: {}\n".format(modified_message.decode(), address, port))
+        
+
+if __name__ ==  '__main__':
+    main()
 
 
 
