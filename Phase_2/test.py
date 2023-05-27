e0e9b4927407b158892ea66d7c22ff91c5c5dc9e
Areyes <areyez92@gmail.com>
2023-05-27 01:29:58 -0400

    deleted mtchat

--- Phase_2/test.py
+++ Phase_2/test.py
@@ -1,15 +1,11 @@
 # Reliable Data Transfer over as Perfectly Reliable Channel: rdt1.0
-from tqdm import tqdm
-from time import sleep
-for i in tqdm(range(1000), unit= "Kb", ascii="-*"):
-    sleep(0.01)
+import os.path
 
-def rdt_send():
-    pass
-def rdt_recv():
-    pass
-def deliver_data():
-    pass
-def make_pkt(data):
-    # Creates a packet containing the data
-    pass
+
+
+save_path = os.getcwd() + '\\src'
+filename = "tedastn.bmp"
+print(save_path)
+
+data = open(os.path.join(save_path, filename), 'w')
+data.close()
