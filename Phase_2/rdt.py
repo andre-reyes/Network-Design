fad0c2103605ba2f251be9b2aba49c0077eb3964
Areyes <areyez92@gmail.com>
2023-05-29 19:27:50 -0400

    updated rdt and server to exchange more info

--- Phase_2/rdt.py
+++ Phase_2/rdt.py
@@ -4,6 +4,7 @@
 # rdt_send() ) by the upper-layer application.
 
 from socket import *
+from time import sleep
 host = "localhost"
 port = 9876
 packet_size = 1024 # 1024 bits == 1 Kb
@@ -11,39 +12,43 @@ sequence_number = 0 # counter for packet number
 
 
 # sending side
+
 def rdt_send(data, filename): # Called from client side to Send data to rdt module for parsing into packets
     udp_client = socket(AF_INET, SOCK_DGRAM)
     udp_client.connect((host, port))
+    print('Sending '+ filename)
 
-    packet_list = make_packets(data, filename)
+
+    packet_list = make_packets(data)
+    udp_client.sendto(str(filename).encode(), (host, port))
+    
+    #send data and then send EOF tag to let server know it's done
     for packet in packet_list:
         udp_client.sendto(packet, (host, port))
-    
+        
+    udp_client.sendto(b'<EDF>', (host, port))
     server_message = udp_client.recv(packet_size)
     print(server_message.decode())
     
 
-    
-    
-
-def make_packets(data, filename): # Creates a packet containing the data
+def make_packets(data): # Creates a packet containing the data
     packet_list = []
     packet = data.read(packet_size)
    
     while packet:
-        packet_list.append(filename.encode())
         packet_list.append(packet)
         packet = data.read(packet_size)
     return packet_list
     
 
-
 # Receiving side 
+def rdt_rcv(packet_list):
+    file_path = "received_files\\resssceived.bmp" 
+    for packet in packet_list:
+        with open(file_path, 'ab') as data:
+            data.write(packet)
 
-def rdt_recv(packet, filename): # called from the server side to recieve a packet
-    file_path = "received_files\\" + filename 
-    with open(file_path, 'ab') as data:
-        data.write(packet)
+
     
               
 
--- Phase_2/server.py
+++ Phase_2/server.py
@@ -3,17 +3,22 @@ from rdt import *
 local_port = 9876         # Port number chosen from range of values (49152-65535) for private/temporary purposes
 bufferSize = 1024          # 
 filename = "receivedFile.bmp" # default filename
+packet_list = []
 
 #create server socket
 server_socket = socket(AF_INET, SOCK_DGRAM)
 server_socket.bind(("",local_port))
 
 print("The server is ready to receive on port {}".format(local_port))
-
+filename, client_address = server_socket.recvfrom(bufferSize)
 #infinite loop to always be listening to port while active
 while True:
-    filename, client_address = server_socket.recvfrom(bufferSize)
     packet, client_address = server_socket.recvfrom(bufferSize)
-    server_socket.sendto("received file!".encode(), client_address)
-    rdt_recv(packet, filename.decode())
+    if packet == b'<EDF>':
+        server_socket.sendto("received file!".encode(), client_address)
+        server_socket.close()
+        break
+    else:
+        packet_list.append(packet)
+rdt_rcv(packet_list)
     
